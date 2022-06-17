c0d157575949f9b8c805d158f861f1ce90b9129d
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-06-16 17:42:53 -0700

    More comments

--- src/core/TriangleSplitter.js
+++ src/core/TriangleSplitter.js
@@ -2,6 +2,7 @@ import { Triangle, Line3, Vector3, Plane } from 'three';
 import { ExtendedTriangle } from 'three-mesh-bvh';
 
 const EPSILON = 1e-14;
+const COPLANAR_EPSILON = 1e-7;
 const _edge = new Line3();
 const _foundEdge = new Line3();
 const _vec = new Vector3();
@@ -9,8 +10,7 @@ const _planeNormal = new Vector3();
 const _plane = new Plane();
 const _triangle = new ExtendedTriangle();
 
-// TODO: this could operate on polygons instead to limit the number of
-// objects and edges to check, then triangulate at the end
+// A pool of triangles to avoid unnecessary triangle creation
 class TrianglePool {
 
 	constructor() {
@@ -47,6 +47,7 @@ class TrianglePool {
 
 }
 
+// Utility class for splitting triangles
 export class TriangleSplitter {
 
 	constructor() {
@@ -57,6 +58,7 @@ export class TriangleSplitter {
 
 	}
 
+	// initialize the class with a triangle
 	initialize( tri ) {
 
 		const { triangles, trianglePool, normal } = this;
@@ -69,50 +71,48 @@ export class TriangleSplitter {
 
 	}
 
+	// Split the current set of triangles by passing a single triangle in. If the triangle is
+	// coplanar it will attempt to split by the triangle edge planes
 	splitByTriangle( triangle ) {
 
 		const { normal } = this;
 		triangle.getPlane( _plane );
 
+		if ( Math.abs( 1.0 - Math.abs( _plane.normal.dot( normal ) ) ) < COPLANAR_EPSILON ) {
 
-		if ( Math.abs( 1.0 - Math.abs( _plane.normal.dot( normal ) ) ) < 1e-7 ) {
+			// if the triangle is coplanar then split by the edge planes
+			const arr = [ triangle.a, triangle.b, triangle.c ];
+			for ( let i = 0; i < 3; i ++ ) {
 
-			if ( triangle === null ) {
+				const nexti = ( i + 1 ) % 3;
 
-				console.warn( 'TriangleSplitter: cannot split using a coplanar plane and no triangle.' );
+				const v0 = arr[ i ];
+				const v1 = arr[ nexti ];
 
-			} else {
+				_vec.subVectors( v1, v0 ).normalize();
+				_planeNormal.crossVectors( normal, _vec );
+				_plane.setFromNormalAndCoplanarPoint( _planeNormal, v0 );
 
-				const arr = [ triangle.a, triangle.b, triangle.c ];
-				for ( let i = 0; i < 3; i ++ ) {
-
-					const nexti = ( i + 1 ) % 3;
-
-					const v0 = arr[ i ];
-					const v1 = arr[ nexti ];
-
-					_vec.subVectors( v1, v0 ).normalize();
-					_planeNormal.crossVectors( normal, _vec );
-					_plane.setFromNormalAndCoplanarPoint( _planeNormal, v0 );
-
-					this.splitByPlane( _plane, triangle );
-
-				}
+				this.splitByPlane( _plane, triangle );
 
 			}
 
 		} else {
 
+			// otherwise split by the triangle plane
 			this.splitByPlane( _plane, triangle );
 
 		}
 
 	}
 
+	// Split the triangles by the given plan. If a triangle is provided then we ensure we
+	// intersect the triangle before splitting the plane
 	splitByPlane( plane, triangle = null ) {
 
 		const { triangles, trianglePool } = this;
 
+		// init our triangle to check for intersection
 		let splittingTriangle = null;
 		if ( triangle !== null ) {
 
@@ -122,11 +122,13 @@ export class TriangleSplitter {
 
 		}
 
+		// try to split every triangle in the class
 		for ( let i = 0, l = triangles.length; i < l; i ++ ) {
 
 			const tri = triangles[ i ];
 			const { a, b, c } = tri;
 
+			// skip the triangle if we don't intersect with it
 			if ( splittingTriangle && ! splittingTriangle.intersectsTriangle( tri ) ) {
 
 				continue;
@@ -140,10 +142,13 @@ export class TriangleSplitter {
 			const arr = [ a, b, c ];
 			for ( let t = 0; t < 3; t ++ ) {
 
-				const tn = ( t + 1 ) % 3;
+				// get the triangle edge
+				const tNext = ( t + 1 ) % 3;
 				_edge.start.copy( arr[ t ] );
-				_edge.end.copy( arr[ tn ] );
+				_edge.end.copy( arr[ tNext ] );
 
+				// track if the start point sits on the plane or if it's on the positive side of it
+				// so we can use that information to determine whether to split later.
 				const distance = plane.distanceToPoint( _edge.start );
 				if ( Math.abs( distance ) < EPSILON ) {
 
@@ -155,14 +160,18 @@ export class TriangleSplitter {
 
 				}
 
+				// check if we intersect the plane (ignoring the start point so we don't double count)
 				if ( plane.intersectLine( _edge, _vec ) && ! _vec.equals( _edge.start ) ) {
 
+					// if we intersect at the end point then we track that point as one that we
+					// have to split down the middle
 					if ( _vec.equals( _edge.end ) ) {
 
 						vertexSplitEnd = t;
 
 					}
 
+					// track the split edge
 					if ( intersects === 0 ) {
 
 						_foundEdge.start.copy( _vec );
@@ -179,6 +188,10 @@ export class TriangleSplitter {
 
 			}
 
+			// skip splitting if:
+			// - we have two points on the plane then the plane intersects the triangle exactly on an edge
+			// - the plane does not intersect on 2 points
+			// - the intersection edge is too small
 			if ( onPlane < 2 && intersects === 2 && _foundEdge.distance() > EPSILON ) {
 
 				if ( vertexSplitEnd !== - 1 ) {
@@ -257,7 +270,7 @@ export class TriangleSplitter {
 
 			} else if ( intersects === 3 ) {
 
-				// console.warn( 'TriangleClipper: Coplanar clip not handled' );
+				console.warn( 'TriangleClipper: Coplanar clip not handled' );
 
 			}
 
