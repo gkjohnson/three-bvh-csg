82b84afd7c5c4ee893a86bd5a6f06777f49d577c
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-06-23 09:19:48 -0700

    Add support for checkerboard parameter

--- src/materials/CsgGridMaterial.js
+++ src/materials/CsgGridMaterial.js
@@ -1,12 +1,20 @@
-import { MeshPhongMaterial } from 'three';
+import { MeshPhongMaterial, Color } from 'three';
 import { csgGridShaderMixin } from './shaderUtils.js';
 
 export class CsgGridMaterial extends MeshPhongMaterial {
 
+	constructor( params ) {
+
+		super();
+		this.checkerboardColor = new Color();
+		this.setValues( params );
+
+	}
+
 	onBeforeCompile( shader ) {
 
-
 		csgGridShaderMixin( shader );
+		shader.uniforms.checkerboardColor.value = this.checkerboardColor;
 
 	}
 
