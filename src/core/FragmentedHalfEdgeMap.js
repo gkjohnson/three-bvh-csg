605a94038bf1a408fb88d44246774f0f1f34387c
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-01-16 22:49:01 +0900

    progress

--- src/core/FragmentedHalfEdgeMap.js
+++ src/core/FragmentedHalfEdgeMap.js
@@ -1,9 +1,12 @@
 import { Ray, Line3, Vector3 } from 'three';
 import { HalfEdgeMap } from './HalfEdgeMap.js';
 
+const EPSILON = 1e-10;
 const _ray = new Ray();
-const _ray2 = new Ray();
+const _reverseRay = new Ray();
+const _distanceRay = new Ray();
 const _line = new Line3();
+const _vec = new Vector3();
 
 function toNormalizedRay( v0, v1, ray ) {
 
@@ -25,6 +28,108 @@ function hashEdge( v0, v1 ) {
 function hashRay( ray ) {
 
 	return hashEdge( ray.origin, ray.direction );
+
+}
+
+function removeOverlap( arr, a, connectionMap = null ) {
+
+	for ( let i = 0; i < arr.length; i ++ ) {
+
+		const b = arr[ i ];
+		if ( a.end < b.start ) {
+
+			continue;
+
+		} else if ( a.start > b.end ) {
+
+			break;
+
+		} else if ( a.start < b.start ) {
+
+			if ( a.end > b.end ) {
+
+				// a extends over b on both ends
+				// should never get here if our mesh is formed correctly
+				b.start = b.end;
+
+			} else {
+
+				const tmp = a.end;
+				a.end = b.start;
+				b.start = tmp;
+
+			}
+
+		} else if ( b.start < a.start ) {
+
+			if ( b.end > a.end ) {
+
+				// b is longer than a
+				const toInsert = {
+					index: b.index,
+					start: a.end,
+					end: b.end,
+				};
+
+				b.end = a.start;
+				a.start = a.end;
+				arr.splice( i, 0, toInsert );
+
+			} else {
+
+				const tmp = a.end;
+				a.end = b.start;
+				b.start = tmp;
+
+			}
+
+		}
+
+		if ( b.end - b.start <= EPSILON ) {
+
+			arr.splice( i, 1 );
+			i --;
+
+		}
+
+		if ( connectionMap ) {
+
+			connectionMap.addConnection( a.index, b.index );
+
+		}
+
+		if ( a.end - a.start <= EPSILON ) {
+
+			return true;
+
+		}
+
+	}
+
+	return a.end - a.start <= EPSILON;
+
+}
+
+class EdgeMap extends Map {
+
+	addConnection( a, b ) {
+
+		if ( ! this.has( a ) ) {
+
+			this.set( a, new Set() );
+
+		}
+
+		if ( ! this.has( b ) ) {
+
+			this.set( b, new Set() );
+
+		}
+
+		this.get( a ).add( b );
+		this.get( b ).add( a );
+
+	}
 
 }
 
@@ -71,25 +176,46 @@ export class FragmentedHalfEdgeMap {
 
 			// get a normalized ray
 			toNormalizedRay( _line.start, _line.end, _ray );
+			toNormalizedRay( _line.end, _line.start, _reverseRay );
+
+			const hash = hashRay( _ray );
+			const reverseHash = hashRay( _reverseRay );
+			if ( edgeDistanceMap.has( hash ) ) {
+
+				_distanceRay.copy( edgeDistanceMap.get( reverseHash ).ray );
+
+			} else {
+
+				_distanceRay.copy( _ray );
+
+			}
 
 			// add a distance bounces for the edge
-			const hash = hashRay( _ray );
 			if ( ! edgeDistanceMap.has( hash ) ) {
 
-				toNormalizedRay( _line.end, _line.start, _ray2 );
 				edgeDistanceMap.set( hash, {
-					inverseHash: hashRay( _ray2 ),
+					ray: _ray.clone(),
+					inverseHash: hashRay( _reverseRay ),
 					edges: [],
 				} );
 
 			}
 
 			const list = edgeDistanceMap.get( hash );
+			let start = _vec.subVectors( _line.start, _distanceRay.origin ).dot( _distanceRay.direction );
+			let end = _vec.subVectors( _line.end, _distanceRay.origin ).dot( _distanceRay.direction );
+			if ( end < start ) {
+
+				const tmp = start;
+				start = end;
+				end = tmp;
+
+			}
+
 			list.edges.push( {
-				triIndex,
-				edgeIndex,
-				start: _line.start.distanceTo( _ray.origin ),
-				end: _line.end.distanceTo( _ray.origin ),
+				index: value,
+				start,
+				end,
 			} );
 
 		}
@@ -98,15 +224,21 @@ export class FragmentedHalfEdgeMap {
 
 			const { reverseHash, edges } = value;
 			const otherEdges = edgeDistanceMap.get( reverseHash ).edges;
-			otherEdges.sort( ( a, b ) => {
+			edges.sort( ( a, b ) => a.start - b.start );
+			otherEdges.sort( ( a, b ) => a.start - b.start );
 
-				return a.start - b.start;
+			for ( let i = 0; i < edges.length; i ++ ) {
 
-			} );
+				const fullyMatched = removeOverlap(	edges[ i ], otherEdges );
+				if ( fullyMatched ) {
 
-			// TODO: iterate over edges and check alignment
-			// TODO: is it the case that the distances will just be reversed?
+					edges.splice( i, 1 );
+					i --;
 
+				}
+
+
+			}
 
 		}
 
