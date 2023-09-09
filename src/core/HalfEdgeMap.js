200293f2508a964e41931991d4f45b84b9b6b4be
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-09-09 15:37:51 +0200

    Update disjoint edge map logic

--- src/core/HalfEdgeMap.js
+++ src/core/HalfEdgeMap.js
@@ -1,21 +1,39 @@
 import { Vector2, Vector3, Vector4 } from 'three';
-import { hashNumber, hashVertex2, hashVertex3, hashVertex4 } from '../utils/hashUtils.js';
+import { hashNumber, hashVertex2, hashVertex3, hashVertex4, toNormalizedRay } from '../utils/hashUtils.js';
 import { getTriCount } from './utils.js';
+import { Ray } from 'three';
 
 const _vec2 = new Vector2();
 const _vec3 = new Vector3();
 const _vec4 = new Vector4();
 const _hashes = [ '', '', '' ];
 
+function toTriIndex( v ) {
+
+	return ~ ~ ( v / 3 )
+
+}
+
+function toEdgeIndex( v ) {
+
+	return v % 3;
+
+}
+
 export class HalfEdgeMap {
 
 	constructor( geometry = null ) {
 
+		// result data
 		this.data = null;
-		this.unmatchedEdges = null;
-		this.matchedEdges = null;
+		this.disjointData = null;
+		this.unmatchedEdges = - 1;
+		this.matchedEdges = - 1;
+
+		// options
 		this.useDrawRange = true;
 		this.useAllAttributes = false;
+		this.matchDisjointEdges = false;
 
 		if ( geometry ) {
 
@@ -25,6 +43,7 @@ export class HalfEdgeMap {
 
 	}
 
+	// TODO: can these be faster for use in the CSG operations
 	getSiblingTriangleIndex( triIndex, edgeIndex ) {
 
 		const otherIndex = this.data[ triIndex * 3 + edgeIndex ];
@@ -41,7 +60,7 @@ export class HalfEdgeMap {
 
 	updateFrom( geometry ) {
 
-		const { useAllAttributes, useDrawRange } = this;
+		const { useAllAttributes, useDrawRange, matchDisjointEdges } = this;
 		const hashFunction = useAllAttributes ? hashAllAttributes : hashPositionAttribute;
 
 		// runs on the assumption that there is a 1 : 1 match of edges
@@ -134,6 +153,46 @@ export class HalfEdgeMap {
 		}
 
 		// TODO: iterate over the unmatched set of edges
+		if ( matchDisjointEdges ) {
+
+			const disjointConnectivityMap = new Map();
+			const fragmentMap = new Map();
+			const edges = Array.from( unmatchedSet );
+			const v0 = new Vector3();
+			const v1 = new Vector3();
+			const ray = new Ray();
+			for ( let i = 0, l = edges.length; i < l; i ++ ) {
+
+				const ei = edges[ i ];
+				const triIndex = toTriIndex( ei );
+				const edgeIndex = toEdgeIndex( ei );
+
+				let i0 = 3 * triIndex + edgeIndex;
+				let i1 = ( i0 + 1 ) % 3;
+				if ( indexAttr ) {
+
+					i0 = indexAttr.getX( i0 );
+					i1 = indexAttr.getX( i1 );
+
+				}
+
+				v0.fromBufferAttribute( posAttr, i0 );
+				v1.fromBufferAttribute( posAttr, i1 );
+
+				// TODO: how can we make this ray always point in the same direction?
+				// First non zer component must be positive? Use two ray hashes?
+				// Dot with a common vector? What about perpendicular vectors?
+				// Or use the winding direction of the triangle somehow?
+				toNormalizedRay( v0, v1, ray );
+
+				// TODO: initialize a set of rays that line on the ray
+				// TODO: append the "stride" along the ray that the current ray covers along with its index.
+				// or if theres already siblings we detect then "eat" away at both strides, removing if
+				// they've shrunk to a tolerance and define a connectivity graph.
+
+			}
+
+		}
 
 		this.matchedEdges = matchedEdges;
 		this.unmatchedEdges = unmatchedSet.size;
--- src/utils/hashUtils.js
+++ src/utils/hashUtils.js
@@ -1,6 +1,6 @@
-const HASH_MULTIPLIER = 1e6;
 const HASH_WIDTH = 1e-6;
 const HASH_HALF_WIDTH = HASH_WIDTH * 0.5;
+const HASH_MULTIPLIER = - Math.log10( HASH_WIDTH );
 const HASH_ADDITION = HASH_HALF_WIDTH * HASH_MULTIPLIER;
 export function hashNumber( v ) {
 
@@ -26,9 +26,28 @@ export function hashVertex4( v ) {
 
 }
 
-export function hashDirection( v ) {
+export function hashRay( r ) {
 
-	const l = v.length();
-	return `${ hashNumber( v.x / l ) },${ hashNumber( v.y / l ) },${ hashNumber( v.z / l ) }`;
+	return `${ hashVertex3( r.origin ) }-${ hashVertex3( r.direction ) }`;
+
+}
+
+export function toNormalizedRay( v0, v1, target ) {
+
+	// get a normalized direction
+	target
+		.direction
+		.subVectors( v1, v0 )
+		.normalize();
+
+	// project the origin onto the perpendicular plane that
+	// passes through 0, 0, 0
+	const scalar = v0.dot( target.direction );
+	target.
+		origin
+		.copy( v0 )
+		.addScaledVector( target.direction, - scalar );
+
+	return target;
 
 }
