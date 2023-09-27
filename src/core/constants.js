8c96eca2b776c2026846ebf042e5e541d166de74
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-09-27 15:08:12 -0700

    Comment

--- src/core/constants.js
+++ src/core/constants.js
@@ -4,5 +4,6 @@ export const REVERSE_SUBTRACTION = 2;
 export const INTERSECTION = 3;
 export const DIFFERENCE = 4;
 
+// guaranteed non manifold results
 export const HOLLOW_SUBTRACTION = 5;
 export const HOLLOW_INTERSECTION = 6;
