6c224435cef79bbe0b939982fd6bc9df3734c794
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-10-20 20:24:03 +0900

    Add classes

---
+++ src/core/splitter/GraphConnection.js
@@ -0,0 +1,13 @@
+export class GraphConnection {
+
+	constructor() {
+
+		this.start = - 1;
+		this.end = - 1;
+		this.required = false;
+		this.triangle1 = null;
+		this.triangle2 = null;
+
+	}
+
+}
---
+++ src/core/splitter/GraphTriangle.js
@@ -0,0 +1,9 @@
+export class GraphTriangle {
+
+	constructor() {
+
+		this.edges = [];
+
+	}
+
+}
