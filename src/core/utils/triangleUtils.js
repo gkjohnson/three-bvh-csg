e04118fefb7e6dd236f9a2567ef083a81c5a12f9
Garrett Johnson <garrett.kjohnson@gmail.com>
2024-01-02 23:24:07 +0900

    Fix other degenerate issue

--- src/core/utils/triangleUtils.js
+++ src/core/utils/triangleUtils.js
@@ -10,7 +10,7 @@ export function isTriDegenerate( tri, eps = EPSILON ) {
 	// compute angles to determine whether they're degenerate
 	_AB.subVectors( tri.b, tri.a );
 	_AC.subVectors( tri.c, tri.a );
-	_CB.subVectors( tri.c, tri.b );
+	_CB.subVectors( tri.b, tri.c );
 
 	const angle1 = _AB.angleTo( _AC );				// AB v AC
 	const angle2 = _AB.angleTo( _CB );				// AB v BC
