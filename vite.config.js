5d158c798fccc9d727abe4def7a88a974969ad84
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-09-06 00:55:07 +0900

    Add vite config

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { searchForWorkspaceRoot } from 'vite';
+import fs from 'fs';
+
+export default {
+
+	root: './examples/',
+	base: '',
+	build: {
+		outDir: './bundle/',
+		rollupOptions: {
+			input: fs
+				.readdirSync( './examples/' )
+				.filter( p => /\.html$/.test( p ) )
+				.map( p => `./examples/${ p }` ),
+		},
+	},
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot( process.cwd() ),
+			],
+		},
+	}
+
+};
