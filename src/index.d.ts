ab2b324d43a6b02c88be31912c58ee74946dcaee
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-02-07 21:44:54 +0900

    Fix d ts linting

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,27 +2,27 @@ import { BufferGeometry, Group, InstancedMesh, Line3, LineSegments, Mesh, MeshPh
 
 export class Brush extends Mesh {
 
-  isBrush: boolean;
-  markUpdated(): void;
-  isDirty(): boolean;
-  prepareGeometry(): void;
-  disposeCacheData(): void;
+	isBrush: boolean;
+	markUpdated(): void;
+	isDirty(): boolean;
+	prepareGeometry(): void;
+	disposeCacheData(): void;
 
 }
 
 export class TypedAttributeData {
 
-  groupAttributes: Object[];
-  groupCount: number;
+	groupAttributes: object[];
+	groupCount: number;
 
-  getType( name: String ): string;
-  getTotalLength( name: String ): number;
-  getGroupSet( index?: number ): Object;
-  getGroupAttrArray( name: string, index?: number ): Array<number>;
-  initializeArray( name: string, type: string ): void;
-  clear(): void;
-  delete( key: string ): void;
-  reset(): void;
+	getType( name: string ): string;
+	getTotalLength( name: string ): number;
+	getGroupSet( index?: number ): object;
+	getGroupAttrArray( name: string, index?: number ): Array<number>;
+	initializeArray( name: string, type: string ): void;
+	clear(): void;
+	delete( key: string ): void;
+	reset(): void;
 
 }
 
@@ -37,81 +37,81 @@ export const HOLLOW_INTERSECTION: CSGOperation;
 
 export class Evaluator {
 
-  attributes: String[];
-  useGroups: boolean;
-  consolidateMaterials: boolean;
-  debug: OperationDebugData;
+	attributes: string[];
+	useGroups: boolean;
+	consolidateMaterials: boolean;
+	debug: OperationDebugData;
 
-  evaluate( a: Brush, b: Brush, operation: CSGOperation, targetBrush?: Brush ): Brush;
-  evaluate( a: Brush, b: Brush, operations: CSGOperation[], targetBrushes: Brush[] ): Brush[];
+	evaluate( a: Brush, b: Brush, operation: CSGOperation, targetBrush?: Brush ): Brush;
+	evaluate( a: Brush, b: Brush, operations: CSGOperation[], targetBrushes: Brush[] ): Brush[];
 
-  evaluateHierarchy( root: Operation, target?: Brush ): Brush;
+	evaluateHierarchy( root: Operation, target?: Brush ): Brush;
 
 }
 
 export class Operation extends Brush {
 
-  isOperation: boolean;
-  operation: CSGOperation;
-  markUpdated(): void;
-  isDirty(): boolean;
-  insertBefore( brush: Brush ): void;
-  insertAfter( brush: Brush ): void;
+	isOperation: boolean;
+	operation: CSGOperation;
+	markUpdated(): void;
+	isDirty(): boolean;
+	insertBefore( brush: Brush ): void;
+	insertAfter( brush: Brush ): void;
 
 }
 
 export class OperationGroup extends Group {
 
-  isOperatioinGroup: boolean;
-  markUpdated(): void;
-  isDirty(): boolean;
+	isOperatioinGroup: boolean;
+	markUpdated(): void;
+	isDirty(): boolean;
 
 }
 
 export class CullableTriangle extends Triangle {
 
-  initFrom( other: Triangle ): void;
-  updateSide( plane: Plane, triangle: Triangle, coplanarIndex: number ): void;
+	initFrom( other: Triangle ): void;
+	updateSide( plane: Plane, triangle: Triangle, coplanarIndex: number ): void;
 
 }
 
 export class TrianglePool {
 
-  getTriangle(): Triangle;
-  clear(): void;
-  reset(): void;
+	getTriangle(): Triangle;
+	clear(): void;
+	reset(): void;
 
 }
 
 export class TriangleSplitter {
 
-  trianglePool: TrianglePool;
-  triangles: Triangle[];
-  normal: Vector3;
+	trianglePool: TrianglePool;
+	triangles: Triangle[];
+	normal: Vector3;
 
-  initialize( tri: Triangle ): void;
-  splitByTriangle( triangle: Triangle ): void;
-  splitByPlane( plane: Plane, triangle: Triangle, coplanarIndex: number ): void;
-  reset(): void;
+	initialize( tri: Triangle ): void;
+	splitByTriangle( triangle: Triangle ): void;
+	splitByPlane( plane: Plane, triangle: Triangle, coplanarIndex: number ): void;
+	reset(): void;
 
 }
 
 export class HalfEdgeMap {
 
-  constructor( geometry?: BufferGeometry );
-  getSiblingTriangleIndex( triIndex: number, edgeIndex: number ): number;
-  getSiblingEdgeIndex( triIndex: number, edgeIndex: number ): number;
-  updateFrom( geomtry: BufferGeometry ): void;
+	constructor( geometry?: BufferGeometry );
+	getSiblingTriangleIndex( triIndex: number, edgeIndex: number ): number;
+	getSiblingEdgeIndex( triIndex: number, edgeIndex: number ): number;
+	updateFrom( geomtry: BufferGeometry ): void;
 
 }
 
 export class GridMaterial extends MeshPhongMaterial {
 
-  enableGrid: boolean;
+	enableGrid: boolean;
 
 }
 
-export function getTriangleDefinitions( ...triangles: Triangle[] ): String[];
+export function getTriangleDefinitions( ...triangles: Triangle[] ): string[];
 
 export function logTriangleDefinitions( ...triangles: Triangle[] ): void;
 
@@ -119,65 +119,65 @@ export function generateRandomTriangleColors( geometry: BufferGeometry ): void;
 
 export class TriangleSetHelper extends Group {
 
-  constructor( triangles?: Triangle[] );
+	constructor( triangles?: Triangle[] );
 
-  setTriangles( triangles: Triangle[] ): void;
+	setTriangles( triangles: Triangle[] ): void;
 
 }
 
 export class EdgesHelper extends LineSegments {
 
-  constructor( edges?: Line3[] );
+	constructor( edges?: Line3[] );
 
-  setEdges( edges: Line3[] ): void;
+	setEdges( edges: Line3[] ): void;
 
 }
 
 export class TriangleIntersectData {
 
-  intersects: Object;
-  triangle: Triangle;
+	intersects: object;
+	triangle: Triangle;
 
-  constructor( tri: Triangle );
-  addTriangle( index: number, tri: Triangle ): void;
-  getIntersectArray(): Array<Triangle>;
+	constructor( tri: Triangle );
+	addTriangle( index: number, tri: Triangle ): void;
+	getIntersectArray(): Array<Triangle>;
 
 }
 
 export class TriangleIntersectionSets {
 
-  addTriangleIntersection( ia: number, tribA: Triangle, ib: number, triB: Triangle ): void;
-  getTrianglesAsArray( id?: number ): Array<Triangle>;
-  getTriangleIndices(): Array<number>;
-  getIntersectionIndices( id: number ): void;
-  getIntersectionsAsArray( id?: number, id2?: number ): Array<Triangle>;
+	addTriangleIntersection( ia: number, tribA: Triangle, ib: number, triB: Triangle ): void;
+	getTrianglesAsArray( id?: number ): Array<Triangle>;
+	getTriangleIndices(): Array<number>;
+	getIntersectionIndices( id: number ): void;
+	getIntersectionsAsArray( id?: number, id2?: number ): Array<Triangle>;
 
 }
 
 export class OperationDebugData {
 
-  intersectionEdges: Line3[];
-  enabled: boolean;
+	intersectionEdges: Line3[];
+	enabled: boolean;
 
-  addIntersectingTriangles( ia: number, triA: Triangle, ib: number, triB: Triangle ): void;
-  addEdge( edge: Line3 ): void;
-  reset(): void;
+	addIntersectingTriangles( ia: number, triA: Triangle, ib: number, triB: Triangle ): void;
+	addEdge( edge: Line3 ): void;
+	reset(): void;
 
 }
 
 export class PointsHelper extends InstancedMesh {
 
-  constructor( count?: number, points?: Vector3[] );
+	constructor( count?: number, points?: Vector3[] );
 
-  setPoints( points: Vector3 ): void;
+	setPoints( points: Vector3 ): void;
 
 }
 
 export class HalfEdgeHelper extends EdgesHelper {
 
-  constructor( geometry?: BufferGeometry, halfEdges?: HalfEdgeMap );
-  setHalfEdges( geometry: BufferGeometry, halfEdges: HalfEdgeMap ): void;
+	constructor( geometry?: BufferGeometry, halfEdges?: HalfEdgeMap );
+	setHalfEdges( geometry: BufferGeometry, halfEdges: HalfEdgeMap ): void;
 
 }
 
-export function computeMeshVolume( mesh : Mesh | BufferGeometry ) : Number;
+export function computeMeshVolume( mesh : Mesh | BufferGeometry ) : number;
