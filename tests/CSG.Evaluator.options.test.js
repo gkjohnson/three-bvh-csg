fc99d8a1992e357e77a56d13d5e7a41a09443e46
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-02-10 22:37:17 +0900

    Add tests for new option

--- tests/CSG.Evaluator.options.test.js
+++ tests/CSG.Evaluator.options.test.js
@@ -10,6 +10,7 @@ describe( 'Evaluator', () => {
 			const evaluator = new Evaluator();
 			evaluator.useGroups = true;
 			evaluator.consolidateGroups = false;
+			evaluator.removeUnusedMaterials = false;
 
 			const brush1 = new Brush( new BoxGeometry() );
 			brush1.updateMatrixWorld();
@@ -29,6 +30,7 @@ describe( 'Evaluator', () => {
 			const evaluator = new Evaluator();
 			evaluator.useGroups = false;
 			evaluator.consolidateGroups = false;
+			evaluator.removeUnusedMaterials = false;
 
 			const brush1 = new Brush( new BoxGeometry() );
 			brush1.updateMatrixWorld();
@@ -53,6 +55,7 @@ describe( 'Evaluator', () => {
 			const evaluator = new Evaluator();
 			evaluator.useGroups = true;
 			evaluator.consolidateGroups = true;
+			evaluator.removeUnusedMaterials = true;
 
 			const brush1 = new Brush( new BoxGeometry() );
 			brush1.updateMatrixWorld();
@@ -72,6 +75,7 @@ describe( 'Evaluator', () => {
 			const evaluator = new Evaluator();
 			evaluator.useGroups = true;
 			evaluator.consolidateGroups = false;
+			evaluator.removeUnusedMaterials = true;
 
 			const brush1 = new Brush( new BoxGeometry() );
 			brush1.geometry.groups.forEach( g => g.materialIndex = 0 );
@@ -85,6 +89,50 @@ describe( 'Evaluator', () => {
 			const result = evaluator.evaluate( brush1, brush2, SUBTRACTION );
 			expect( result.material ).toHaveLength( 2 );
 			expect( result.geometry.groups ).toHaveLength( 12 );
+
+		} );
+
+	} );
+
+	describe( 'removeUnusedMaterials', () => {
+
+		it( 'should remove unused materials when true.', () => {
+
+			const evaluator = new Evaluator();
+			evaluator.useGroups = true;
+			evaluator.consolidateGroups = true;
+			evaluator.removeUnusedMaterials = true;
+
+			const brush1 = new Brush( new BoxGeometry() );
+			brush1.updateMatrixWorld();
+
+			const brush2 = new Brush( new BoxGeometry() );
+			brush2.rotation.set( Math.PI / 4, 0, Math.PI / 4 );
+			brush2.updateMatrixWorld();
+
+			const result = evaluator.evaluate( brush1, brush2, SUBTRACTION );
+			expect( result.material ).toHaveLength( 2 );
+			expect( result.geometry.groups ).toHaveLength( 2 );
+
+		} );
+
+		it( 'should leave unused materials when false.', () => {
+
+			const evaluator = new Evaluator();
+			evaluator.useGroups = true;
+			evaluator.consolidateGroups = true;
+			evaluator.removeUnusedMaterials = false;
+
+			const brush1 = new Brush( new BoxGeometry() );
+			brush1.updateMatrixWorld();
+
+			const brush2 = new Brush( new BoxGeometry() );
+			brush2.rotation.set( Math.PI / 4, 0, Math.PI / 4 );
+			brush2.updateMatrixWorld();
+
+			const result = evaluator.evaluate( brush1, brush2, SUBTRACTION );
+			expect( result.material ).toHaveLength( 12 );
+			expect( result.geometry.groups ).toHaveLength( 2 );
 
 		} );
 
