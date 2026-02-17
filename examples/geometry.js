918ecea381f26557292ef55710e439ecafeba76a
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-02-17 10:34:57 +0900

    Add consistent random function

--- examples/geometry.js
+++ examples/geometry.js
@@ -252,6 +252,17 @@ function updateCSG() {
 
 function randomizeBrushes() {
 
+	// function mulberry32(seed) {
+	// 	return function() {
+	// 		let t = (seed += 0x6D2B79F5);
+	// 		t = Math.imul(t ^ (t >>> 15), t | 1);
+	// 		t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+	// 		return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+	// 	};
+	// }
+
+	// Math.random = surfaceSampler.randomFunction = mulberry32( 0 );
+
 	for ( let i = 0; i < brushes.length; i ++ ) {
 
 		const b = brushes[ i ];
